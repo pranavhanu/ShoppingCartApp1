89376bcfadae6fae9602d45fe0485da87e063178
Pranav Hanumanthu <pranavhanu01@gmail.com>
2023-08-31 10:59:03 +0530

    first commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
 export function About() {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>Hello, this is a shopping cart application made by Pranav</h2>
+    </div>
+  );
 }
